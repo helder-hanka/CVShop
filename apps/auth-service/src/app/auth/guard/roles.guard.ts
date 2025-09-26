ede7538b9b89ce571c92bd9c2e5a1327403bde00
Helder <hfernandes238@gmail.com>
2025-09-26 14:33:43 +0200

    feat(auth-service): Add roles.guard

---
+++ apps/auth-service/src/app/auth/guard/roles.guard.ts
@@ -0,0 +1,30 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Observable } from 'rxjs';
+import { ROLES_KEY } from '../decorators/roles.decorator';
+import { Role } from '@cvshop/shared-dto';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+  canActivate(
+    ctx: ExecutionContext
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
+      ctx.getHandler(),
+      ctx.getClass(),
+    ]);
+    if (!requiredRoles) return true;
+    const req = ctx.switchToHttp().getRequest();
+    const user = req.user as { role: Role; status: string };
+    if (!user) throw new ForbiddenException('Not authenticated');
+    if (user.status === 'SUSPENDED')
+      throw new ForbiddenException('Account suspended');
+    return requiredRoles.includes(user.role);
+  }
+}
