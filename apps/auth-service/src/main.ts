0d58fe88a554fab61dfdec1903bcd2fe60bf40c9
Helder <hfernandes238@gmail.com>
2025-09-26 11:43:52 +0200

    feat(auth-service): Add useGlobalPipes

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -6,9 +6,11 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   // const port = process.env.PORT || 3000;
