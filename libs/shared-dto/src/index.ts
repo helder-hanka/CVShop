56f96cf3b97e1cc0392afac7c6605c04a7d3178d
Helder <hfernandes238@gmail.com>
2025-09-30 12:16:21 +0200

    feat(auth-service): add Export events, login.token.dto

--- libs/shared-dto/src/index.ts
+++ libs/shared-dto/src/index.ts
@@ -1,3 +1,5 @@
 export * from './lib/shared-dto';
 export * from './auth/roles.enum';
 export * from './auth/jwt-payload.interface';
+export * from './notifications/events';
+export * from './auth/login.token.dto';
