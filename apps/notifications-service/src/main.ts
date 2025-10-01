13d800c95a127a30868f0b8cc6da0a90d3904f7d
Helder <hfernandes238@gmail.com>
2025-10-01 10:49:32 +0200

    feat(notification-service): Up env port

--- apps/notifications-service/src/main.ts
+++ apps/notifications-service/src/main.ts
@@ -12,7 +12,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+  const port = process.env.NOTIFICATIONS_PORT
+    ? Number(process.env.NOTIFICATIONS_PORT)
+    : 3005;
   // Microservice RMQ
   const rmqUser =
     process.env.RABBITMQ_URL ?? 'amqp://guest:guest@localhost:5672';
