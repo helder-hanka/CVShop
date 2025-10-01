e39a3f968a7dc32a4f3f76bc9f000ecabbd682d8
Helder <hfernandes238@gmail.com>
2025-10-01 10:12:00 +0200

    feat(notifications-service): add Microservice RMQ

--- apps/notifications-service/src/main.ts
+++ apps/notifications-service/src/main.ts
@@ -6,13 +6,26 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT ? Number(process.env.PORT) : 3000;
-  await app.listen(port);
+  // Microservice RMQ
+  const rmqUser =
+    process.env.RABBITMQ_URL ?? 'amqp://guest:guest@localhost:5672';
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: [rmqUser],
+      queue: 'notifications',
+      queueOptions: { durable: true },
+    },
+  });
+  // await app.listen(port);
+  await app.startAllMicroservices();
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
   );
