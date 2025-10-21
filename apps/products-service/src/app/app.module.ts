34f6b2621b17b5647423200b70b38b6ea250d0d7
Helder <hfernandes238@gmail.com>
2025-10-21 15:58:10 +0200

    feat(products): implement Product and Category entities with DTOs and controller

--- apps/products-service/src/app/app.module.ts
+++ apps/products-service/src/app/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { envValidationSchema } from '@cvshop/shared-utils';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Product } from './products/entities/product.entity';
+import { Category } from './products/entities/category.entity';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: envValidationSchema,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST ?? 'localhost',
+      port: Number(process.env.DB_PORT ?? 5432),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      entities: [Product, Category],
+      synchronize: false,
+    }),
+    ProductsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/products-service/src/main.ts
+++ apps/products-service/src/main.ts
@@ -3,16 +3,38 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  // const port = process.env.PORT || 3000;
-  const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+
+  const port = process.env.PRODUCTS_PORT
+    ? Number(process.env.PRODUCTS_PORT)
+    : 3000;
+
+  const config = new DocumentBuilder()
+    .setTitle('Products Service')
+    .setDescription('The Auth Service API description')
+    .setVersion('1.0')
+    // .addTag('auth')
+    // .addBearerAuth(
+    //   {
+    //     type: 'http',
+    //     scheme: 'bearer',
+    //     bearerFormat: 'JWT',
+    //   },
+    //   'access-token'
+    // )
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
